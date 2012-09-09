7106224ef5e603b6cf109b2df8819cd92fba62a6
Joni Kanerva <joni@devlab.fi>
2012-09-09 11:24:23 +0300

    script for updating shell

--- README.md
+++ README.md
@@ -1,24 +1,14 @@
-## Terminal preferences
+# Installing new machine
 
+## Homebrew
+    ruby <(curl -fsSkL raw.github.com/mxcl/homebrew/go)
+    brew update
+    brew doctor
+    brew install zsh git hub ssh-copy-id wget grc htop
+
+## Z shell
     chsh -s /bin/zsh
-
-    git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-    git clone git://github.com/jonikanerva/dotfiles.git ~/.dotfiles
-    cp ~/.dotfiles/joni.zsh-theme ~/.oh-my-zsh/custom/
-    cp ~/.dotfiles/.zshrc ~/.zshrc
-    cp ~/.dotfiles/.gitconfig ~/.gitconfig
-    cp ~/.dotfiles/.gitignore ~/.gitignore
-    cp ~/.dotfiles/.gemrc ~/.gemrc
-    cp ~/.dotfiles/.irbrc ~/.irbrc
-    cp ~/.dotfiles/.pryrc ~/.pryrc
-    cp -R ~/.dotfiles/.bundle ~/.bundle
     rm -f ~/.bash*
 
-## Sublime Text 2 preferences
-
-    cd ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User
-    cp ~/.dotfiles/Default\ \(OSX\).sublime-keymap .
-    cp ~/.dotfiles/Preferences.sublime-settings .
-    wget -q https://raw.github.com/ryanb/textmate-themes/master/railscasts.tmTheme
-    wget -q https://raw.github.com/deplorableword/textmate-solarized/master/Solarized%20(dark).tmTheme
-    wget -q https://raw.github.com/chriskempson/tomorrow-theme/master/textmate/Tomorrow-Night-Eighties.tmTheme
+## Terminal Settings
+    curl --silent -L https://raw.github.com/jonikanerva/dotfiles/master/terminal-setup.sh | bash
---
+++ terminal-setup.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/bash
+
+echo
+echo "Setting up terminal"
+echo
+
+# Check we have Git
+command -v git > /dev/null
+
+if [[ $? != 0 ]]; then
+  echo "Install git first!"
+  exit
+fi
+
+# Check we have Curl
+command -v curl > /dev/null
+
+if [[ $? != 0 ]]; then
+  echo "Install curl first!"
+  exit
+fi
+
+# Check .bundle dir
+if [ ! -d ~/.bundle ]; then
+  mkdir ~/.bundle
+fi
+
+# Fetch oh-my-zsh
+if [ ! -d ~/.oh-my-zsh ]; then
+  echo "Getting oh-my-zsh.."
+  git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh --quiet
+fi
+
+echo "Getting .rc files.."
+
+if [ -d ~/.oh-my-zsh ]; then
+  cd ~/.oh-my-zsh/custom
+  curl --silent -O https://raw.github.com/jonikanerva/dotfiles/master/joni.zsh-theme
+fi
+
+cd ~
+curl --silent -O https://raw.github.com/jonikanerva/dotfiles/master/.zshrc
+curl --silent -O https://raw.github.com/jonikanerva/dotfiles/master/.gitignore
+curl --silent -O https://raw.github.com/jonikanerva/dotfiles/master/.gitconfig
+curl --silent -O https://raw.github.com/jonikanerva/dotfiles/master/.irbrc
+curl --silent -O https://raw.github.com/jonikanerva/dotfiles/master/.pryrc
+curl --silent -O https://raw.github.com/jonikanerva/dotfiles/master/.gemrc
+curl --silent -O https://raw.github.com/jonikanerva/dotfiles/master/.bundle/config
+
+# If we have Sublime Text
+if [ -d ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User ]; then
+  echo "Getting Sublime Text preferences.."
+  cd ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User
+  curl --silent -O https://raw.github.com/jonikanerva/dotfiles/master/Preferences.sublime-settings
+  curl --silent -O https://raw.github.com/jonikanerva/dotfiles/master/Default%20\(OSX\).sublime-keymap
+  curl --silent -O https://raw.github.com/ryanb/textmate-themes/master/railscasts.tmTheme
+  curl --silent -O https://raw.github.com/deplorableword/textmate-solarized/master/Solarized%20\(dark\).tmTheme
+  curl --silent -O https://raw.github.com/chriskempson/tomorrow-theme/master/textmate/Tomorrow-Night-Eighties.tmTheme
+fi
+
+echo
+echo "Done."
+echo
