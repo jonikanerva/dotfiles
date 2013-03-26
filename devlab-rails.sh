784ee003181c9c2ca7fcfd31d93ce66d9cb48fcf
Joni Kanerva <joni@devlab.fi>
2013-03-26 17:42:20 +0200

    vaan ihan pakolliset rc filet

--- devlab-rails.sh
+++ devlab-rails.sh
@@ -8,8 +8,6 @@ if [ ! -d ~/.bundle ]; then
 fi
 
 curl --silent -o ~/.gitignore      https://raw.github.com/jonikanerva/dotfiles/master/.gitignore
-curl --silent -o ~/.irbrc          https://raw.github.com/jonikanerva/dotfiles/master/.irbrc
-curl --silent -o ~/.pryrc          https://raw.github.com/jonikanerva/dotfiles/master/.pryrc
 curl --silent -o ~/.gemrc          https://raw.github.com/jonikanerva/dotfiles/master/.gemrc
 curl --silent -o ~/.bundle/config  https://raw.github.com/jonikanerva/dotfiles/master/.bundle/config
 
