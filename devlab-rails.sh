7c510fb22c927b01de89b12336e7c58e729c0a74
Joni Kanerva <joni@devlab.fi>
2013-11-12 17:03:29 +0200

    add bash_profile stuff

--- devlab-rails.sh
+++ devlab-rails.sh
@@ -1,15 +1,25 @@
 #!/usr/bin/bash
 
 echo
-echo -n "Installing required .rc files..."
+echo -n "Installing required configs for bundle, gem and rbenv... "
 
 if [ ! -d ~/.bundle ]; then
   mkdir ~/.bundle
 fi
 
-curl --silent -o ~/.gitignore      https://raw.github.com/jonikanerva/dotfiles/master/.gitignore
 curl --silent -o ~/.gemrc          https://raw.github.com/jonikanerva/dotfiles/master/.gemrc
 curl --silent -o ~/.bundle/config  https://raw.github.com/jonikanerva/dotfiles/master/.bundle/config
 
-echo "OK"
+echo '
+# Add rbenv to path
+export PATH="$HOME/.rbenv/bin:$PATH"
+
+# Load rbenv
+if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
+
+# Add alias for updating rbenv
+alias rbenv-update="cd ~/.rbenv && git pull && cd ~/.rbenv/plugins/ruby-build && git pull && cd"
+' >> ~/.bash_profile
+
+echo "OK!"
 echo
