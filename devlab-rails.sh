6ccefde08639902ee0db345bf36aac54478cb960
Joni Kanerva <joni@devlab.fi>
2014-08-26 10:37:05 +0300

    only for mac

--- devlab-rails.sh
+++ devlab-rails.sh
@@ -10,16 +10,13 @@ fi
 curl --silent -o ~/.gemrc          https://raw.githubusercontent.com/jonikanerva/dotfiles/master/.gemrc
 curl --silent -o ~/.bundle/config  https://raw.githubusercontent.com/jonikanerva/dotfiles/master/.bundle/config
 
-echo '
+cat > ~/.bash_profile << EOF
 # Add rbenv to path
 export PATH="$HOME/.rbenv/bin:$PATH"
 
 # Load rbenv
-if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
-
-# Add alias for updating rbenv
-alias rbenv-pull="cd ~/.rbenv && git pull && cd ~/.rbenv/plugins/ruby-build && git pull && cd ~/.rbenv/plugins/rbenv-gem-rehash && git pull && cd"
-' >> ~/.bash_profile
+if command -v rbenv &> /dev/null; then eval "\$(rbenv init -)"; fi
+EOF
 
 echo "OK!"
 echo
